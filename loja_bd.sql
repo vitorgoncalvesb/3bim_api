8f71275d354707d584639762bda1ee58235f24b8
vitorgoncalvesb <vitor.goncalvesb1@gmail.com>
2026-08-20 08:45:04 -0300

    correção erro bd

--- loja_bd.sql
+++ loja_bd.sql
@@ -7,6 +7,9 @@
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
+CREATE DATABASE IF NOT EXISTS `loja`;
+USE `loja`;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
